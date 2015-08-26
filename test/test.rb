7ea382be5d4aee5d4bb50a6b5fd7be4f2725c1d3
Chris <claymanchris@gmail.com>
2015-08-25 19:10:31 -0700

    reformatting of tests to bacon

--- test/test.rb
+++ test/test.rb
@@ -19,63 +19,30 @@ describe "Hack Reactioner" do
 
   it 'responds to index' do
     get '/'
-    last_response.should.be.ok?
+    last_response.should.be.ok
     last_response.body.should.include('Hack Reactions')
   end
 
+  it 'shows all of the questions' do
+    get '/question/all'
+    last_response.should.be.ok
+
+  end
+
+  it 'creates new questions' do
+    get '/question/new'
+    last_response.should.be.ok
+    last_response.body.should.include('Add a Question')
+  end
+
+  it 'shows a created question' do
+    q = CORE::Question.new(:title=>'Does this work?')
+    q.save!
+    id = CORE::Question.last.id
+    get '/question/show/' + id.to_s
+    last_response.should.be.ok
+    last_response.body.should.include('Does this work?')
+    CORE::Question.find(id).delete
+  end
+
 end
-
-
-#
-#
-# class ReactionerText < MiniTest::Unit::TestCase
-#   include Rack::Test::Methods
-#
-#   def app
-#     CORE::Main
-#   end
-#
-#   def test_index
-#     get '/'
-#     assert last_response.ok?
-#     assert last_response.body.include?('Hack Reactions')
-#   end
-#
-#   def test_new_question_form
-#     get '/question/new'
-#     assert last_response.ok?
-#
-#   end
-#
-#   def test_list_questions
-#     get '/question/all'
-#     assert last_response.ok?
-#   end
-#
-#   def test_test_creation
-#     get '/question/new'
-#     assert last_response.ok?
-#     assert last_response.body.include('Add a Question')
-#
-#
-#     post '/question/create'
-#
-#
-#   end
-#
-#   def test_show_question
-#     q = CORE::Question.new(:title=>'Does this work?')
-#     q.save!
-#     id = CORE::Question.last.id
-#     puts id.to_s
-#     get '/question/show/' + id.to_s
-#     assert last_response.ok?
-#     assert last_response.body.include?('Does this work?')
-#     CORE::Question.find(id).delete
-#   end
-#
-#
-#
-#
-#
-# end
